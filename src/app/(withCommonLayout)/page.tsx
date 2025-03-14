ac9aa0333fe55ade695b753a27d775b29a1b13c1
Mohosin <mohosinali075@gmail.com>
2025-03-14 23:21:53 +0600

    specialist section design and data load

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/ui/homePage/heroSection/HeroSection";
+import Specialist from "@/components/ui/homePage/specialist/Specialist";
 const HomePage = () => {
   return (
-    <div>
+    <>
       <HeroSection/>
-    </div>
+      <Specialist/>
+    </>
   );
 };
 
